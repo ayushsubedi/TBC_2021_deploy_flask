880b5876b1068a9a2ed18ef933507b87bd0761b3
Ayush Subedi <ayush.subedi@gmail.com>
2021-01-05 09:42:18 +0545

    complete deployment poc for jan5th

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
+import pickle
 from flask import Flask, request
-from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder as le
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
@@ -39,33 +39,44 @@ def get_features():
     return  {'carat': carat, 'cut':cut, 'color':color, 'clarity':clarity, 'x':x, 'y':y, 'z':z}, 200
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/get_diamond_price')
+def get_diamond_price():
+    carat = request.args.get("carat")
+    cut = request.args.get("cut")
+    color = request.args.get("color")
+    clarity = request.args.get("clarity")
+    x = request.args.get("x")
+    y = request.args.get("y")
+    z = request.args.get("z")
+    df = pd.DataFrame([{'carat': carat, 'cut':cut, 'color':color, 'clarity':clarity, 'x':x, 'y':y, 'z':z}])
+    model = pickle.load(open('static/saved_models/randomforestregressor.pkl', 'rb'))
+    cut_le = pickle.load(open('static/saved_models/cut_le.pkl', 'rb'))
+    color_le= pickle.load(open('static/saved_models/color_le.pkl', 'rb'))
+    clarity_le = pickle.load(open('static/saved_models/clarity_le.pkl', 'rb'))
+    df['cut'] = cut_le.transform(df['cut'])
+    df['color'] = color_le.transform(df['color'])
+    df['clarity'] = clarity_le.transform(df['clarity'])
+    result = model.predict(df)[0]
+    return  {"result": result, "input":{'carat': carat, 'cut':cut, 'color':color, 'clarity':clarity, 'x':x, 'y':y, 'z':z}}
 
 
 @app.route('/train_model')
 def train_model():
     df = pd.read_csv('static/datasets/diamonds.csv')
-    df['cut'] = preprocessing.LabelEncoder().fit_transform(df['cut'])
-    df['color'] = preprocessing.LabelEncoder().fit_transform(df['color'])
-    df['clarity'] = preprocessing.LabelEncoder().fit_transform(df['clarity'])
+    cut_le, color_le, clarity_le = le(), le(), le()
+    df['cut'] = cut_le.fit_transform(df['cut'])
+    df['color'] = color_le.fit_transform(df['color'])
+    df['clarity'] = clarity_le.fit_transform(df['clarity'])
+    pickle.dump(cut_le, open('static/saved_models/cut_le.pkl', 'wb'))
+    pickle.dump(color_le, open('static/saved_models/color_le.pkl', 'wb'))
+    pickle.dump(clarity_le, open('static/saved_models/clarity_le.pkl', 'wb'))
     df.drop(['depth', 'table'], axis=1, inplace=True)
     X = df.drop('price', axis=1) 
     y = df.price
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
     model= RandomForestRegressor()
     model = model.fit(X_train, y_train)
+    pickle.dump(model, open('static/saved_models/randomforestregressor.pkl', 'wb'))
     return 'model was built sucessfully'
 
 
